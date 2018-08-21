2e18c9fe60e9c20554af658cb5e6656056ccd18c
Afham Abid <noefeah89@gmail.com>
2018-08-21 15:01:26 +0530

    Refactoring

---
+++ app/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+
+    <title>PHP Web App</title>
+</head>
+
+<body>
+    <h2>PHP Web App</h2>
+    <?php echo "Hello World!"; ?>
+    <p>
+        <a href="https://github.com/afhm"> Boilerplate made by afhm</a>
+    </p>
+</body>
+
+</html>

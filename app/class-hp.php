d292a80e79e16d330bc9a40a3f3f8a43346625e3
Mohammed Afham <noefeah89@gmail.com>
2018-08-27 20:13:13 +0530

    edited php

--- app/class-hp.php
+++ app/class-hp.php
@@ -4,7 +4,7 @@ $query = $_GET['query'];
 $connect = mysqli_connect("127.0.0.1", "root", "");
 mysqli_select_db($connect, "food");
 
-$fetch1 = mysqli_query($connect, "SELECT * FROM location");
+$fetch1 = mysqli_query($connect, "SELECT name FROM location");
 
 // set array
 $array = array();
